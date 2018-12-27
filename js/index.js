ec664c0f11fc293a3d52cce08cf1465250a2cfb7
inderjot.singh <inderjot.singh@mountblue.io>
2018-12-27 10:08:12 +0530

    functionality done remaining with oop

--- js/index.js
+++ js/index.js
@@ -1,47 +1,91 @@
-var mytable = "<table cellpadding=\"0\" cellspacing=\"0\"><tbody><tr>";
+var mytable 
 var currentId;
 var selectedValues = [];
 var selectedIds = [];
 var targetIds = []
 var sum = 0
-var rows= 7
-var columns=7
-for (var i = 1; i <= rows; i++) {
-    let col = "";
-    for (var j = 1; j <= columns; j++) {
-        // console.log(i + "" + j)
-        col += `<td ` + "id=" + i + "" + j + ` ` + "onclick=selectValue(" + i + "" + j + ")" + `> 
-                <input type="text" name="FirstName" value=` + i + j + `>  </td>`
-    }
-    mytable += "<tr>" + col + "</tr>";
-}
+var rows = 7
+var columns = 7
+var div
 
-mytable += "</tr" + "onclick=getRowIndex(this)" + "></table>";
-document.write(mytable);
-function  showTable() {
-        for (var i = 1; i <= rows; i++) {
-        let col = "";
+
+
+function showTable() {
+    // var myTable = document.getElementById("table");
+    // console.log(myTable)
+    // myTable.removeChild(myTable.childNodes[0])
+    var table = document.getElementById('table');
+    console.log(table)
+    var tbl = document.createElement('table');
+    tbl.style.width = '100%';
+    tbl.setAttribute('border', '1');
+    var tbdy = document.createElement('tbody');
+    for (var i = 1; i <= rows; i++) {
+        var tr = document.createElement('tr');
         for (var j = 1; j <= columns; j++) {
-            // console.log(i + "" + j)
-            col += `<td ` + "id=" + i + "" + j + ` ` + "onclick=getValue(" + i + "" + j + ")" + `> 
-                <input type="text" name="FirstName" value=` + i + j + `>  </td>`
-        }
-        mytable += "<tr>" + col + "</tr>";
-    }
 
-    mytable += "</tr" + "onclick=getRowIndex(this)" + "></table>";
-    document.write(mytable);
+            var td = document.createElement('td');
+            td.setAttribute("id", i + "" + j);
+            td.setAttribute('onclick', " selectValue(" + i + "" + j + ")")
+            var input = document.createElement('input')
+            input.setAttribute('value', i + "" + j)
+            td.appendChild(input)
+            tr.appendChild(td)
+
+        }
+        tbdy.appendChild(tr);
+    }
+    tbl.appendChild(tbdy);
+    table.appendChild(tbl)
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('hi')
+    // div = document.getElementById('setCol');
+    // // debugger
+    // div.insertAdjacentHTML('afterend', mytable)
+    var table = document.getElementById('table');
+    var tbl = document.createElement('table');
+    tbl.style.width = '100%';
+    tbl.setAttribute('border', '1');
+    var tbdy = document.createElement('tbody');
+    for (var i = 1; i <= rows; i++) {
+        var tr = document.createElement('tr');
+        for (var j = 1; j <= columns; j++) {
+
+            var td = document.createElement('td');
+            td.setAttribute("id", i + "" + j);
+            td.setAttribute('onclick', " selectValue(" + i + "" + j + ")")
+            var input = document.createElement('input')
+            input.setAttribute('value', i + "" + j)
+            td.appendChild(input)
+            tr.appendChild(td)
+
+        }
+        tbdy.appendChild(tr);
+    }
+    tbl.appendChild(tbdy);
+    table.appendChild(tbl)
+})
 
 // setter for rows and columns
-var setRows=(Rows)=>{
-    
-    rows=Rows
-    console.log(Rows)
+var setRows = (Rows) => {
+    rows = Rows
+     var myTable = document.getElementById("table");
+     while (myTable.hasChildNodes()) {
+         myTable.removeChild(myTable.firstChild);
+     }
+    //  console.log(myTable.childNodes[0])
+    //  myTable.removeChild(myTable.childNodes[0])
     showTable()
+
 }
 var setColumns = (Columns) => {
     columns = Columns
+    var myTable = document.getElementById("table");
+    while (myTable.hasChildNodes()) {
+        myTable.removeChild(myTable.firstChild);
+    }
     showTable()
 }
 
@@ -63,6 +107,8 @@ var selectValue = (id) => {
 
 // key press events
 var KeyPress = (e) => {
+    // e.stopPropagation();
+    // e.preventDefault();
     var evtobj = window.event ? event : e
     var Row = document.getElementById(currentId);
     var Cells = Row.getElementsByTagName("input");
@@ -70,31 +116,32 @@ var KeyPress = (e) => {
     if (evtobj.keyCode == 67 && evtobj.ctrlKey) { // on copy
         // console.log(currentId);
         if (selectedIds.length != 0) {
+            console.log(selectedIds)
             var textarea = document.createElement("textarea");
             textarea.textContent = selectedIds;
             textarea.style.position = "fixed";
             document.body.appendChild(textarea);
+            // console.log(textarea)
             textarea.select();
             try {
-                
-                return document.execCommand("copy");
+                return document.getElementById('clipboard').innerHTML = getSelection()
+                // return document.execCommand("copy");
             } catch (ex) {
                 console.warn("Copy to clipboard failed.", ex);
                 return false;
             } finally {
-                selectedIds = [];
                 document.body.removeChild(textarea);
             }
         } else {
             // console.log(Cells[0]);
             Cells[0].select();
-            document.execCommand("copy");
+            return document.getElementById('clipboard').innerHTML = getSelection()
         }
 
     } else if (evtobj.keyCode == 88 && evtobj.ctrlKey) { // on cut
         // console.log(currentId);
         if (selectedIds.length != 0) {
-
+            var cutCells = []
             selectedIds.forEach((id, index) => {
                 // console.log(id)
                 document.getElementById(id).style.border = "1px solid #0000FF";
@@ -102,10 +149,12 @@ var KeyPress = (e) => {
                 let tag = idTag.getElementsByTagName('input')
                 tag[0].select()
                 // tag.select()
-                document.execCommand('cut')
-                
+                cutCells.push(getSelection().toString())
+                document.execCommand("cut");
             })
-            
+            console.log(cutCells)
+            document.getElementById('clipboard').innerHTML = cutCells
+
         } else {
             // console.log(Cells[0]);
             Cells[0].select();
@@ -122,12 +171,12 @@ var KeyPress = (e) => {
                 document.getElementById(id).style.border = "1px solid #0000FF";
             })
         }
-    } else if (evtobj.ctrlKey && evtobj.keyCode == 86 && targetIds.length == 0 ) { // paste
+    } else if (evtobj.ctrlKey && evtobj.keyCode == 86 && targetIds.length == 0 && document.getElementById('clipboard').innerHTML != 0) { // paste
         // let id = document.getElementById(11)
         // let tag = id.getElementsByTagName('input')
         // tag[0].value()
         // document.execCommand("paste");
-        
+        console.log(document.getElementById('clipboard').innerHTML.length)
         e.stopPropagation();
         e.preventDefault();
         targetIds.push(currentId)
@@ -137,6 +186,7 @@ var KeyPress = (e) => {
                 targetIds.push(targetIds[0] + (selectedIds[index] - selectedIds[0]))
             }
         })
+        document.getElementById('clipboard').innerHTML = null
         console.log(targetIds)
         console.log(selectedValues)
         targetIds.forEach((id, index) => {
@@ -144,16 +194,26 @@ var KeyPress = (e) => {
             let tag = idTag.getElementsByTagName('input')
             tag[0].value = selectedValues[index]
         })
-        sumOfSelected()
+        selectedIds.forEach((id, index) => {
+            // console.log(id)
+            document.getElementById(id).style.border = 'none';
+        })
+        selectedIds = []
+        selectedValues = []
+        targetIds = []
     }
+
 }
 
 var sumOfSelected = () => {
+    sum = 0;
     selectedIds.forEach((value) => {
-        sum += value
+        let idTag = document.getElementById(value)
+        let tag = idTag.getElementsByTagName('input')
+        sum += JSON.parse(tag[0].value)
     })
     console.log(sum)
-    sum = 0;
+    document.getElementById('Sum').value = sum
 }
 document.onkeydown = KeyPress;
 document.onclick = KeyPress;
