be21d3b233d56ae688412a1eab1b3bbf014e151f
inderjot.singh <inderjot.singh@mountblue.io>
2019-01-04 15:59:48 +0530

    drag and select done. bugs in overlay selection div

--- js/newIndex.js
+++ js/newIndex.js
@@ -5,14 +5,18 @@ var targetIds = []
 var sum = 0
 var Rows
 var Cells
+var initialId
+var finalId
 
 function Cell() {
     this.color = 'white'
     this.row
     this.column
-    
+
     var cell = document.createElement('div')
     cell.style.width = 'max-content';
+    cell.style.position = 'relative'
+
     cell.setAttribute('border', '1');
     cell.setAttribute('value', '1');
     cell.setAttribute('onmouseover', 'onMouseOver()')
@@ -21,12 +25,8 @@ function Cell() {
     input.style.textAlign = 'center';
     input.style.background = this.color
 
-    this.setColor = function (color) {
-        this.color = color
-        input.style.background = color
-    }
     this.setInputOnclick = function (data) {
-        console.log(data)
+        // console.log(data)
     }
     this.setInputValue = function (value) {
         input.setAttribute('value', value)
@@ -37,6 +37,7 @@ function Cell() {
     }
 
     this.setId = function (id) {
+        // console.log(id)
         cell.setAttribute("id", id);
     }
 
@@ -51,12 +52,67 @@ function Cell() {
     this.getCell = function () {
         return cell
     }
+    this.appendChild = function (id) {
+        console.log('here')
+        var selectionCell = new SelectionCell()
+        selectionCell.setId(id)
+        selectionCell.setBackground(id)
+        selectionCell.setInputValue(id)
+        selectionCell.appendInput()
+        var select = selectionCell.getCell()
+        // console.log(select)
+        cell.style.zIndex = '1'
+        cell.style.opacity='0.9'
+        cell.appendChild(select)
+    }
+
+}
+
+function SelectionCell() {
+    this.color = 'cyan'
+    this.row
+    this.column
+    var cell = document.createElement('div')
+    cell.style.width = 'max-content';
+    cell.style.height='100%'
+    cell.style.position = 'relative'
+    cell.setAttribute('border', '1');
+    cell.setAttribute('value', '1');
+    cell.setAttribute('onmouseover', 'onMouseOver()')
+    var input = document.createElement('input')
+    input.style.width = '100%';
+    input.style.background='cyan'
+    input.style.textAlign = 'center';
+    input.style.background = this.color
+    this.setInputValue = function (value) {
+        // input.setAttribute('value', value)
+        
+    }
+    this.setId = function (id) {
+        // console.log(id)
+        cell.setAttribute("id", id);
+    }
+    this.appendInput = function () {
+        cell.appendChild(input)
+    }
+    this.getCell = function () {
+        return cell
+    }
+    this.setBackground = function (id) {
+
+        cell.style.background = '#a0c3ff'
+        cell.style.zIndex = '0'
+        cell.style.opacity = '0.20'
+        cell.style.top = ' 0px'
+        cell.style.left = ' 0px'
+        cell.style.position = 'absolute'
+        cell.style.display='none'
+    }
 }
 
 function Table() {
     this.data = []
-    this.rows
-    this.columns
+    this.idRowColumn = {}
     this.getRows = function () {
 
         return parseInt(this.rows)
@@ -78,6 +134,7 @@ function Table() {
         save.saveColumns(column)
     };
     this.makeTable = function () {
+        this.idRowColumn = {}
         var table = document.getElementById('table');
         var oldTable = document.getElementById("table");
         while (oldTable.hasChildNodes()) {
@@ -88,22 +145,39 @@ function Table() {
             for (var j = 1; j <= this.columns; j++) {
                 // console.log("hi")
                 var newCell = new Cell()
+                // var selectionCell = new SelectionCell()
+                // selectionCell.setId(i + "" + j + i + "" + j)
+                // selectionCell.setBackground(i + "" + j + i + "" + j)
+                // selectionCell.setInputValue(i + "" + j + i + "" + j)
+                // selectionCell.appendInput()
                 newCell.setId(i + "" + j)
                 newCell.setOnClick(i + "" + j)
                 newCell.setInputValue(i + "" + j)
                 newCell.setInputOnChange()
                 newCell.setInputOnclick(newCell)
                 newCell.appendInput()
+                newCell.appendChild(i + "" + j + i + "" + j)
                 var cell = newCell.getCell()
+                // var selectionCell = selectionCell.getCell()
+                // console.log(i + "" + j + i + "" + j)
                 // console.log(cell)
+
                 row.appendChild(cell);
+                // row.appendChild(selectionCell)
                 row.style.display = 'flex'
+                let idRow = i
+                let idColumn = j
+                this.idRowColumn[i + '' + j] = {
+                    'row': idRow,
+                    'column': idColumn
+                }
             }
             // console.log(i)
             table.appendChild(row)
         }
     };
     this.savedTable = function (data) {
+        this.idRowColumn = {}
         var count = 0
         console.log(this.rows)
         console.log(this.columns)
@@ -118,16 +192,8 @@ function Table() {
             var row = document.createElement("div");
             for (var j = 1; j <= this.columns; j++) {
                 // console.log('inside saved table')
-                var td = document.createElement('td');
-                td.setAttribute("id", i + "" + j);
-                td.setAttribute('onclick', " selectValue(" + i + "" + j + ")")
-                var input = document.createElement('input')
-                input.style.width = '100%';
-                input.style.textAlign = 'center';
-
                 var newCell = new Cell()
-                newCell.setId(i + "" + j)
-                newCell.setOnClick(i + "" + j)
+                
                 // console.log((i + "" + j) + 1)
                 // debugger
                 // console.log(data[count]);
@@ -140,18 +206,26 @@ function Table() {
 
                     newCell.setInputValue(i + "" + j)
                 }
+
                 newCell.setInputOnChange()
                 newCell.appendInput()
+                newCell.appendChild(i + "" + j + i + "" + j)
                 var cell = newCell.getCell()
+                
                 // console.log(cell)
                 row.appendChild(cell);
+                row.appendChild(selectionCell)
                 row.style.display = 'flex'
+                let idRow = i
+                let idColumn = j
+                this.idRowColumn[i + '' + j] = {
+                    'row': idRow,
+                    'column': idColumn
+                }
             }
             // console.log(i)
-
             table.appendChild(row)
         }
-
     };
     this.sum = function () {
         sum = 0;
@@ -243,33 +317,208 @@ var changeColor = (data) => {
 var onMouseOver = () => {
 
     if (window.event.which == 1) {
-        console.log(event.target.parentElement.id)
-        currentId = event.target.parentElement.id;
-        Row = document.getElementById(event.target.parentElement.id);
-        Cells = Row.getElementsByTagName("input");
-        Cells[0].select();
-        if (!selectedValues.includes(Cells[0].value)) {
-            selectedValues.push(Cells[0].value)
-            selectedIds.push(currentId)
-            // console.log(Cells[0])
-            selectedIds.forEach((id, index) => {
-                // console.log(id)
-                document.getElementById(id).style.border = "0.5px solid #0000FF";
-            })
+        event.preventDefault();
+
+
+        var newFinalId = event.target.parentElement.id
+
+        console.log("1", "initial :-", initialId, "newfinal :-", newFinalId, "final :-", finalId)
+        // dragSelect(initialId, newFinalId)
+        // console.log(newTable.idRowColumn[initialId].row, newTable.idRowColumn[initialId].row)
+        // console.log(newTable.idRowColumn[finalId].row, newTable.idRowColumn[finalId].row)
+        if (initialId && newFinalId) {
+            if (!finalId) {
+                finalId = newFinalId
+                console.log("2", newFinalId, finalId)
+            }
+            var initialRow = newTable.idRowColumn[initialId].row
+            var initialCol = newTable.idRowColumn[initialId].column
+            var finalRow = newTable.idRowColumn[finalId].row
+            var finalCol = newTable.idRowColumn[finalId].column
+            var newFinalRow = newTable.idRowColumn[newFinalId].row
+            var newFinalCol = newTable.idRowColumn[newFinalId].column
+
+            if (finalCol >= initialCol && finalRow >= initialRow) { // case 1
+                if (newFinalRow < finalRow) {
+                    //remove selection
+                    removeSelections(newFinalRow, finalRow, 'row')
+                    finalId = newFinalId
+                }
+                if (newFinalCol < finalCol) {
+                    //remove selection
+                    removeSelections(newFinalCol, finalCol, 'column')
+                    finalId = newFinalId
+                }
+                if (newFinalRow >= finalRow) {
+                    //drag and select\
+                    finalId = newFinalId
+                    dragSelect(initialId, finalId)
+                }
+                if (newFinalCol >= finalCol) {
+                    //drag and select
+                    finalId = newFinalId
+                    dragSelect(initialId, finalId)
+                }
+
+            }
+            if (finalCol >= initialCol && finalRow <= initialRow) { // case 2
+                if (newFinalRow > finalRow) {
+                    //remove selection
+                    removeSelections(finalRow, newFinalRow, 'row')
+                    finalId = newFinalId
+                }
+                if (newFinalCol < finalCol) {
+                    //remove selection
+                    removeSelections(newFinalCol, finalCol, 'column')
+                    finalId = newFinalId
+                }
+                if (newFinalRow <= finalRow) {
+                    //drag and select\
+                    finalId = newFinalId
+                    dragSelect(initialId, finalId)
+                }
+                if (newFinalCol >= finalCol) {
+                    //drag and select
+                    finalId = newFinalId
+                    dragSelect(initialId, finalId)
+                }
+
+            }
+            if (finalCol <= initialCol && finalRow <= initialRow) { // case 3 
+                console.log('inside case 3 ')
+                if (newFinalRow > finalRow) {
+                    //remove selection
+                    removeSelections(finalRow, newFinalRow, 'row')
+                    finalId = newFinalId
+                }
+                if (newFinalCol > finalCol) {
+                    //remove selection
+                    removeSelections(finalCol, newFinalCol, 'column')
+                    finalId = newFinalId
+                }
+                if (newFinalRow <= finalRow) {
+                    //drag and select\
+                    finalId = newFinalId
+                    dragSelect(initialId, finalId)
+                }
+                if (newFinalCol <= finalCol) {
+                    //drag and select
+                    finalId = newFinalId
+                    dragSelect(initialId, finalId)
+                }
+
+            }
+            if (finalCol <= initialCol && finalRow >= initialRow) { // case 4
+                if (newFinalRow < finalRow) {
+                    //remove selection
+                    removeSelections(newFinalRow, finalRow, 'row')
+                    finalId = newFinalId
+                }
+                if (newFinalCol > finalCol) {
+                    //remove selection
+                    removeSelections(finalCol, newFinalCol, 'column')
+                    finalId = newFinalId
+                }
+                if (newFinalRow >= finalRow) {
+                    //drag and select\
+                    finalId = newFinalId
+                    dragSelect(initialId, finalId)
+                }
+                if (newFinalCol <= finalCol) {
+                    //drag and select
+                    finalId = newFinalId
+                    dragSelect(initialId, finalId)
+                }
+
+            }
+
+
         }
+
     }
 }
+var removeSelections = (start, end, type) => {
+
+    console.log("inside remove", start, end, type)
+
+
+    for (i = start + 1; i <= end; i++) {
+        var copy = selectedIds
+        copy.forEach((a) => {
+            // console.log('22',newTable.idRowColumn['22'][type] ==i)
+            // console.log('a',newTable.idRowColumn[a][type] == i)
+            console.log(selectedIds, a)
+            if (newTable.idRowColumn[a][type] == i) {
+                // console.log("check", selectedIds, a)
+                var index = selectedIds.indexOf(a)
+                // console.log(index)
+                if (index > -1) {
+                    // console.log("here")
+                    selectedIds.splice(index, 1);
+                    selectedValues.splice(index, 1)
+                    console.log(selectedIds, selectedValues)
+                    // document.getElementById(a).style.border = "none";
+                    document.getElementById(a).getElementsByTagName('input')[0].style.background = "#ffffff";
+                }
+            }
+        })
+    }
+
+
+}
+var dragSelect = (initialId, finalId) => {
+    var rowStart = newTable.idRowColumn[initialId].row
+    var rowEnd = newTable.idRowColumn[finalId].row
+    var colStart = newTable.idRowColumn[initialId].column
+    var colEnd = newTable.idRowColumn[finalId].column
+    // console.log(initialId,finalId)
+    // console.log(newTable.idRowColumn[initialId].row, newTable.idRowColumn[initialId].column)
+    // console.log(newTable.idRowColumn[finalId].row, newTable.idRowColumn[finalId].column)
+    if (newTable.idRowColumn[initialId].row > newTable.idRowColumn[finalId].row) {
+        rowStart = newTable.idRowColumn[finalId].row
+        rowEnd = newTable.idRowColumn[initialId].row
+    }
+
+    if (newTable.idRowColumn[initialId].column > newTable.idRowColumn[finalId].column) {
+        colStart = newTable.idRowColumn[finalId].column
+        colEnd = newTable.idRowColumn[initialId].column
+    }
+    for (let i = rowStart; i <= rowEnd; i++) {
+        for (let j = colStart; j <= colEnd; j++) {
+            // console.log("inside loop")
+            Row = document.getElementById(i + '' + j);
+            Cells = Row.getElementsByTagName("input");
+            // console.log(Cells[0].value)
+            Cells[0].select();
+            // console.log(window.getSelection().getRangeAt(0))    imp
+            // window.getSelection().removeRange(window.getSelection().getRangeAt(0))
+            // console.log(window.getSelection())
+
+            //      
+            if (!selectedIds.includes(i + '' + j)) {
+                selectedValues.push(Cells[0].value)
+                selectedIds.push(i + '' + j)
+                // selectedIds.splice(0, 1)    imp
+                // console.log(Cells[0])
+                selectedIds.forEach((id, index) => {
+                    // console.log(id)
+                    // document.getElementById(id).getElementsByTagName('input')[0].style.background = "#0000FF";
+                    document.getElementById(id+id).style.display='block'
+                })
+            }
+        }
+
+    }
+}
+
 var selectValue = (id) => {
     currentId = id;
     Row = document.getElementById(id);
     Cells = Row.getElementsByTagName("input");
-    console.log(Cells[0]);
+    console.log(id);
     Cells[0].select();
-    // document.execCommand("copy");
-    // if (window.event) {
-
-    // }
-    // console.log(window.event.which)
+    // console.log(window.getSelection());
+    initialId = id
 }
 
 
@@ -283,7 +532,7 @@ var KeyPress = (e) => {
     // var Cells = Row.getElementsByTagName("input");
     // console.log(evtobj)
     if (evtobj.keyCode == 67 && evtobj.ctrlKey) { // on copy
-        // console.log(currentId);
+        console.log(currentId);
         if (selectedIds.length != 0) {
             console.log(selectedIds)
             var textarea = document.createElement("textarea");
@@ -302,7 +551,7 @@ var KeyPress = (e) => {
                 document.body.removeChild(textarea);
             }
         } else {
-            // console.log(Cells[0]);
+            console.log(Cells[0]);
             Cells[0].select();
             return document.getElementById('clipboard').innerHTML = getSelection()
         }
@@ -313,7 +562,9 @@ var KeyPress = (e) => {
             var cutCells = []
             selectedIds.forEach((id, index) => {
                 // console.log(id)
-                document.getElementById(id).style.border = "1px solid #0000FF";
+                // document.getElementById(id).getElementsByTagName('input')[0].style.background = "#0000FF";
+                document.getElementById(id+id).style.display='block'
+
                 let idTag = document.getElementById(id)
                 let tag = idTag.getElementsByTagName('input')
                 tag[0].select()
@@ -334,13 +585,14 @@ var KeyPress = (e) => {
         }
 
     } else if (evtobj.ctrlKey && window.event.which == 1) { // ctrl+select(works with mouse)
-        if (!selectedValues.includes(Cells[0].value)) {
+        if (!selectedIds.includes(i + '' + j)) {
             selectedValues.push(Cells[0].value)
             selectedIds.push(currentId)
             // console.log(Cells[0])
             selectedIds.forEach((id, index) => {
                 // console.log(id)
-                document.getElementById(id).style.border = "0.5px solid #0000FF";
+                // document.getElementById(id).getElementsByTagName('input')[0].style.background = "#0000FF";
+                document.getElementById(id+id).style.display='block'
             })
         }
     } else if (evtobj.ctrlKey && evtobj.keyCode == 86 && targetIds.length == 0 && document.getElementById('clipboard').innerHTML != 0) { // paste
@@ -368,7 +620,7 @@ var KeyPress = (e) => {
         })
         selectedIds.forEach((id, index) => {
             // console.log(id)
-            document.getElementById(id).style.border = 'none';
+            document.getElementById(id).getElementsByTagName('input')[0].style.background = "white";
         })
         save.saveCells(newTable.tableData())
         selectedIds = []
